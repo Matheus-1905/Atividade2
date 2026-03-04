0715c6edf44ddeb426f762c4717de569971dbaad
Matheus Moraes <theus190509@gmail.com>
2026-03-03 21:51:36 -0300

    codigo com array e parametros

---
+++ praticando.js
@@ -0,0 +1,9 @@
+function listaMercado(itensCozinha, mantimentos, frutas) {
+
+console.log("-----------------------------------------------------")
+console.log(`fui no mercado e comprei ${itensCozinha[0]}, ${mantimentos[0]}, ${frutas[0]}`)
+console.log(`fui no mercado e comprei ${itensCozinha[1]}, ${mantimentos[1]}, ${frutas[1]}`)
+console.log(`fui no mercado e comprei ${itensCozinha[2]}, ${mantimentos[2]}, ${frutas[2]}`)
+
+}
+listaMercado(["leite", "ovos", "carne"], ["farinha lactea", "arroz","feijão"], ["maçã", "mamão","abacate"])
